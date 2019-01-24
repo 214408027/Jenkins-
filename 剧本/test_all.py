351d7a761f4b30da4223bbb95ca4068c9ec4ce92
214408027 <46983305+214408027@users.noreply.github.com>
2019-01-24 18:11:15 +0800

    Update test_all.py

--- 剧本/test_all.py
+++ 剧本/test_all.py
@@ -1,6 +1,6 @@
 import allure
 
-class Test_allure:
+class TestAllure:
     @allure.step(title = '前置')
     def setup(self):
         print('qianzhi ')
@@ -8,20 +8,15 @@ class Test_allure:
     @allure.step(title = '后置')
     def teardown(self):
         print('houzhi')
-
-
-
+        
     @allure.step(title = '测试登录的流程')
     def test_login(self):
-
         allure.attach('登录', '呵呵哒')
         print('呵呵哒')
-
         assert 1
         
+    @allure.step(title = '测试退出的流程')
     def test_out(self):
-
-    allure.attach('退出', '呵呵哒')
-    print('无敌了')
-
-    assert 1
+        allure.attach('退出', '退出')
+        print('无敌了')
+        assert 1
