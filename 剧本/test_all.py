61c9a099a7a65c4d9d52e01848d2fc6196830f2a
214408027 <46983305+214408027@users.noreply.github.com>
2019-01-24 18:12:12 +0800

    Update test_all.py

--- 剧本/test_all.py
+++ 剧本/test_all.py
@@ -11,12 +11,10 @@ class TestAllure:
         
     @allure.step(title = '测试登录的流程')
     def test_login(self):
-        allure.attach('登录', '呵呵哒')
         print('呵呵哒')
         assert 1
         
     @allure.step(title = '测试退出的流程')
     def test_out(self):
-        allure.attach('退出', '退出')
         print('无敌了')
         assert 1
