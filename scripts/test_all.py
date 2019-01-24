158a805862056679510bd38959d2a9443669e101
heheda <heheda@163.com>
2019-01-24 18:52:06 +0800

    script

---
+++ scripts/test_all.py
@@ -0,0 +1,20 @@
+import allure
+
+class Test_allure:
+    @allure.step(title = '前置')
+    def setup(self):
+        print('qianzhi ')
+
+    @allure.step(title = '后置')
+    def teardown(self):
+        print('houzhi')
+
+
+
+    @allure.step(title = '测试登录的流程')
+    def test_login(self):
+
+        allure.attach('登录', '呵呵哒')
+        print('呵呵哒')
+
+        assert 0
