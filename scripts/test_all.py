612fd4268e41f47dc3f3bbffe411e3eacfd81864
214408027 <46983305+214408027@users.noreply.github.com>
2019-01-24 18:54:20 +0800

    Update test_all.py

--- scripts/test_all.py
+++ scripts/test_all.py
@@ -13,8 +13,12 @@ class Test_allure:
 
     @allure.step(title = '测试登录的流程')
     def test_login(self):
-
         allure.attach('登录', '呵呵哒')
         print('呵呵哒')
+        assert 1
 
-        assert 0
+    @allure.step(title = '测试tuichu的流程')
+    def test_out(self):
+        allure.attach('退出', '呵呵哒')
+        print('汉字个')
+        assert 1
